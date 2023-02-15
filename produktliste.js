4afbb8e7e7ae91e4bc08144d15a5469a92e71e89
Asta Malmgren <asta5093@stud.kea.dk>
2023-02-15 13:50:30 +0100

    udkast til drinkliste + rest db - done

---
+++ produktliste.js
@@ -0,0 +1,34 @@
+const url = "https://personliste-ddf3.restdb.io/rest/drinks";
+
+const options = {
+  headers: {
+    "x-apikey": "63ea0e24478852088da6817a",
+  },
+};
+
+async function hentData() {
+  const respons = await fetch(url, options);
+  const json = await respons.json();
+  vis(json);
+}
+
+const main = document.querySelector("main");
+const template = document.querySelector("template").content;
+
+function vis(json) {
+  console.log(json);
+  json.forEach((drink) => {
+    const klon = template.cloneNode(true);
+    klon.querySelector(".Titel").textContent = drink.navn;
+    klon.querySelector(".beskrivelse").textContent = drink.beskrivelse;
+    klon.querySelector(".tag1").textContent = drink.tag1;
+
+    klon.querySelector(".alkohol").textContent = drink.tag4;
+
+    klon.querySelector(".billede").src = "billeder/" + drink.Billede;
+
+    main.appendChild(klon);
+  });
+}
+
+hentData();
