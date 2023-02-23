6223224c7957378868f2361c01cfe32828b9503d
Asta Malmgren <asta5093@stud.kea.dk>
2023-02-23 10:46:30 +0100

    noter i JS

--- produktliste.js
+++ produktliste.js
@@ -1,19 +1,25 @@
+//søg efter url parameter fra kategoriside
 const urlParams = new URLSearchParams(window.location.search);
 const cat = urlParams.get("cat");
 
 const url = `https://personliste-ddf3.restdb.io/rest/drinks?tag4=${cat}`;
 
+//ændre overskrift til kategori
 document.querySelector("h1").textContent = cat;
 
+//indsend API nøgle for at låse op for Restdb
 const options = {
   headers: {
     "x-apikey": "63ea0e24478852088da6817a",
   },
 };
 
+//Hent data
 async function hentData() {
+  //loope data for hvert produkt
   const respons = await fetch(url, options);
   const json = await respons.json();
+  //Kald næste funktion
   visData(json);
 }
 
@@ -24,6 +30,7 @@ function visData(json) {
   console.log(json);
   json.forEach((drink) => {
     if (cat == "Alle" || cat == drink.tag4) {
+      //
       const klon = template.cloneNode(true);
       klon.querySelector(".Titel").textContent = drink.navn;
       klon.querySelector(".beskrivelse").textContent = drink.beskrivelse;
