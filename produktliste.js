dea5a00a85941ba42c36d87aeb56242d6803243c
Asta Malmgren <asta5093@stud.kea.dk>
2023-02-23 09:44:56 +0100

    kategori og produktliste linked

--- produktliste.js
+++ produktliste.js
@@ -1,4 +1,7 @@
-const url = "https://personliste-ddf3.restdb.io/rest/drinks";
+const urlParams = new URLSearchParams(window.location.search);
+const cat = urlParams.get("cat");
+
+const url = `https://personliste-ddf3.restdb.io/rest/drinks?tag4=${cat}`;
 
 const options = {
   headers: {
@@ -9,13 +12,13 @@ const options = {
 async function hentData() {
   const respons = await fetch(url, options);
   const json = await respons.json();
-  vis(json);
+  visData(json);
 }
 
 const main = document.querySelector("main");
 const template = document.querySelector("template").content;
 
-function vis(json) {
+function visData(json) {
   console.log(json);
   json.forEach((drink) => {
     const klon = template.cloneNode(true);
@@ -23,6 +26,7 @@ function vis(json) {
     klon.querySelector(".beskrivelse").textContent = drink.beskrivelse;
     klon.querySelector(".alkohol").textContent = drink.tag4;
     klon.querySelector(".billede").src = "billeder/" + drink.Billede;
+
     klon.querySelector(".link").href = "produkt.html?id=" + drink._id;
 
     main.appendChild(klon);
