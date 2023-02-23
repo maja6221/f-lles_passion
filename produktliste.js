9714e4729e6b432a4f90b7b42b4318c9b54ee734
Asta Malmgren <asta5093@stud.kea.dk>
2023-02-23 10:51:13 +0100

    Noter i JS færdig

--- produktliste.js
+++ produktliste.js
@@ -24,24 +24,33 @@ async function hentData() {
 }
 
 const main = document.querySelector("main");
+
+//fang template
 const template = document.querySelector("template").content;
 
 function visData(json) {
   console.log(json);
   json.forEach((drink) => {
+    //Sortering af produkter efter alkoholtype (tag4)
     if (cat == "Alle" || cat == drink.tag4) {
-      //
+      //klon template
       const klon = template.cloneNode(true);
+
+      //Skift data
       klon.querySelector(".Titel").textContent = drink.navn;
       klon.querySelector(".beskrivelse").textContent = drink.beskrivelse;
       klon.querySelector(".alkohol").textContent = drink.tag4;
       klon.querySelector(".billede").src = "billeder/" + drink.Billede;
 
+      //konkatener produkt ID og URL parameter
+      //Dette for at forbinde produktliste med det produktsiden når brugeren klikker.
       klon.querySelector(".link").href = "produkt.html?id=" + drink._id;
 
+      // append child
       main.appendChild(klon);
     }
   });
 }
 
+//kald den første funktion
 hentData();
