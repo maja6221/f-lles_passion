cd7168d8be241875b0887339731c92647ad96756
Asta Malmgren <asta5093@stud.kea.dk>
2023-02-23 10:22:52 +0100

    Sortering fungerer

--- produktliste.js
+++ produktliste.js
@@ -3,6 +3,8 @@ const cat = urlParams.get("cat");
 
 const url = `https://personliste-ddf3.restdb.io/rest/drinks?tag4=${cat}`;
 
+document.querySelector("h1").textContent = cat;
+
 const options = {
   headers: {
     "x-apikey": "63ea0e24478852088da6817a",
@@ -21,15 +23,17 @@ const template = document.querySelector("template").content;
 function visData(json) {
   console.log(json);
   json.forEach((drink) => {
-    const klon = template.cloneNode(true);
-    klon.querySelector(".Titel").textContent = drink.navn;
-    klon.querySelector(".beskrivelse").textContent = drink.beskrivelse;
-    klon.querySelector(".alkohol").textContent = drink.tag4;
-    klon.querySelector(".billede").src = "billeder/" + drink.Billede;
+    if (cat == "" || cat == drink.tag4) {
+      const klon = template.cloneNode(true);
+      klon.querySelector(".Titel").textContent = drink.navn;
+      klon.querySelector(".beskrivelse").textContent = drink.beskrivelse;
+      klon.querySelector(".alkohol").textContent = drink.tag4;
+      klon.querySelector(".billede").src = "billeder/" + drink.Billede;
 
-    klon.querySelector(".link").href = "produkt.html?id=" + drink._id;
+      klon.querySelector(".link").href = "produkt.html?id=" + drink._id;
 
-    main.appendChild(klon);
+      main.appendChild(klon);
+    }
   });
 }
 
